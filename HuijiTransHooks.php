4120da80131cc48568bf4d18aa48e4527038c102
reasno <guxi99@gmail.com>
2016-04-13 20:20:42 +0800

    asd

--- HuijiTransHooks.php
+++ HuijiTransHooks.php
@@ -12,9 +12,12 @@ class HuijiTransHooks {
 		return true;
 	}
 	public static function onSkinTemplateToolboxEnd( &$skinTemplate ){
-		$title = SpecialPage::getTitleFor('ManageMessageGroups');
-		$line = Linker::LinkKnown($title, '<i class="fa fa-file-video-o"></i> 创建字幕翻译', array('class'=>'create-srt') );
-		echo Html::rawElement( 'li', array(), $line );
+		global $wgUser;
+		if ($wgUser->isAllowed('translate-manage')){
+			$title = SpecialPage::getTitleFor('ManageMessageGroups');
+			$line = Linker::LinkKnown($title, '<i class="fa fa-file-video-o"></i> 创建字幕翻译', array('class'=>'create-srt') );
+			echo Html::rawElement( 'li', array(), $line );			
+		}
 		return true;
 	}
 }
