9b62944bff9b335ac83cf4147516a647effad348
reasno <guxi99@gmail.com>
2016-05-12 15:45:20 +0800

    add history aid

---
+++ includes/HistoryAid.php
@@ -0,0 +1,44 @@
+<?php
+class HistoryDefinitionAid extends TranslationAid{
+	public function getData(){
+		$page = $this->handle->getKey();
+		$ns = $this->handle->getTitle()->getNamespace();
+		//getHistory
+		$title = $this->handle->getTitle();
+		$latestRevId = $title->getLatestRevID();
+		$lastRev = Revision::newFromId($latestRevId);
+		$i = 0;
+		$value = array();
+		while($lastRev != null){
+			$value[$i]['text'] = $lastRev->getContent->getNativeData();
+			$value[$i]['user'] = $lastRev->getUser()->getName();
+			$i++;
+			$lastRev = $lastRev->getNext();
+		}
+
+		//BuildHtml
+		$templateParser = new TemplateParser(  __DIR__ . '/../Views');
+		
+		$history = $templateParser->processTemplate(
+    		'history',
+    		array(
+        		'value' => $value,
+    		)
+		);
+		return array(
+			'language' => $wgContLang->getCode(),
+			'value' => $i,
+			'html' => $history
+		);
+	}
+	private function getLine($row){
+		$title = 
+
+		$output = '';
+		$rev = new Revision($row);
+		$output .= Linker::revUserTools( $rev, true );
+		$output .= Linker::revComment( $rev, true );
+		return $output;
+
+	}
+}
